a21c662a874215ec2280aa8463531dc4a019abae
norvca <norvca@gmail.com>
2020-08-17 22:03:19 +0800

    feat: add province, company market value

--- app/assets/js/modules/template.js
+++ app/assets/js/modules/template.js
@@ -4,6 +4,8 @@ function template(arr) {
     const increaseRate = e.f3;
     const area = e.f13;
     const stockCode = e.f12;
+    const companyPrice = (e.f20 / 100000000).toFixed(1);
+    const province = e.f102.substr(0, 2);
 
     return `<tr class="hover-bg-washed-green ">
             <td class="pv2  bt br b--black-20 mid-gray">${
@@ -16,11 +18,20 @@ function template(arr) {
                 : `<td class="x-bk pv2  bt br b--black-20 mid-gray">-</td>`
             }
             
+            <td class="pv2  bt br b--black-20 mid-gray">${province}</td>
+
             ${
               area == 0
                 ? `<td class="pv2  bt br b--black-20 mid-gray"><a target="_blank" class="no-underline blue" href="https://xueqiu.com/S/SZ${stockCode}">${e.f14}</a></td>`
                 : `<td class="pv2  bt br b--black-20 mid-gray"><a target="_blank" class="no-underline blue" href="https://xueqiu.com/S/SH${stockCode}">${e.f14}</a></td>`
             }
+
+            ${
+              companyPrice < 200
+                ? `<td class="pv2  bt br b--black-20 mid-gray">${companyPrice}亿</td>`
+                : `<td class="pv2  bt br b--black-20 silver">${companyPrice}亿</td>`
+            }
+
             <td class="pv2  bt br b--black-20 mid-gray">${e.f2}</td>
             ${
               increaseRate >= 0
