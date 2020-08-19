4b6d42d15441b13b22c4c7790f9406c46c0f5296
norvca <norvca@gmail.com>
2020-08-19 11:34:31 +0800

    feat: add concept

--- app/assets/js/modules/template.js
+++ app/assets/js/modules/template.js
@@ -8,42 +8,41 @@ function template(arr) {
     const province = e.f102.substr(0, 2);
 
     return `<tr class="hover-bg-washed-green ">
-            <td class="pv2  bt br b--black-20 mid-gray">${
-              arr.indexOf(e) + 1
-            }</td>
+            <td class="bt br b--black-20 mid-gray ">${arr.indexOf(e) + 1}</td>
 
             ${
               e.bk
-                ? `<td class="x-bk pv2  bt br b--black-20 mid-gray">${e.bk}</td>`
-                : `<td class="x-bk pv2  bt br b--black-20 mid-gray">-</td>`
+                ? `<td class="x-bk pv1  bt br b--black-20 mid-gray ">${e.bk}</td>`
+                : `<td class="x-bk pv1  bt br b--black-20 mid-gray ">-</td>`
             }
             
-            <td class="pv2  bt br b--black-20 mid-gray">${province}</td>
+            <td class="pv1  bt br b--black-20 mid-gray ">${province}</td>
+            <td class="pv1  bt br b--black-20  gray w-10 f7 tl">${e.f103}</td>
 
             ${
               area == 0
-                ? `<td class="pv2  bt br b--black-20 mid-gray"><a target="_blank" class="no-underline blue" href="https://xueqiu.com/S/SZ${stockCode}">${e.f14}</a></td>`
-                : `<td class="pv2  bt br b--black-20 mid-gray"><a target="_blank" class="no-underline blue" href="https://xueqiu.com/S/SH${stockCode}">${e.f14}</a></td>`
+                ? `<td class="pv1  bt br b--black-20 mid-gray "><a target="_blank" class="no-underline blue" href="https://xueqiu.com/S/SZ${stockCode}">${e.f14}</a></td>`
+                : `<td class="pv1  bt br b--black-20 mid-gray "><a target="_blank" class="no-underline blue" href="https://xueqiu.com/S/SH${stockCode}">${e.f14}</a></td>`
             }
 
             ${
               companyPrice < 200
-                ? `<td class="pv2  bt br b--black-20 mid-gray">${companyPrice}亿</td>`
-                : `<td class="pv2  bt br b--black-20 silver">${companyPrice}亿</td>`
+                ? `<td class="pv1  bt br b--black-20 mid-gray ">${companyPrice}亿</td>`
+                : `<td class="pv1  bt br b--black-20 silver ">${companyPrice}亿</td>`
             }
 
-            <td class="pv2  bt br b--black-20 mid-gray">${e.f2}</td>
+            <td class="pv1  bt br b--black-20 mid-gray ">${e.f2}</td>
             ${
               increaseRate >= 0
-                ? `<td class="pv2  bt br b--black-20 red">${e.f3}%</td>`
-                : `<td class="pv2  bt br b--black-20 green">${e.f3}%</td>`
+                ? `<td class="pv1  bt br b--black-20 red ">${e.f3}%</td>`
+                : `<td class="pv1  bt br b--black-20 green ">${e.f3}%</td>`
             }
-            <td class="pv2  bt br b--black-20 mid-gray">${e.f10}</td>
-            <td class="pv2  bt br b--black-20 mid-gray">${e.f8}%</td>
+            <td class="pv1  bt br b--black-20 mid-gray ">${e.f10}</td>
+            <td class="pv1  bt br b--black-20 mid-gray ">${e.f8}%</td>
             ${
               turnOverRateAdd >= 0
-                ? `<td class="pv2 pr3 bt b--black-20 red">${turnOverRateAdd}%</td>`
-                : `<td class="pv2 pr3 bt b--black-20  green">${turnOverRateAdd}%</td>`
+                ? `<td class="pv1 pr3 bt b--black-20 red ">${turnOverRateAdd}%</td>`
+                : `<td class="pv1 pr3 bt b--black-20  green ">${turnOverRateAdd}%</td>`
             }
             
           </tr>`;
